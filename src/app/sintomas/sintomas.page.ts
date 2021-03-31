4b0b2aa84e0f35a3c7415bef05e6d3d659796a5c
Fernanda Alipio <62899970+fernandaalipio@users.noreply.github.com>
2021-03-31 12:34:14 -0300

    Update sintomas.page.ts

--- src/app/sintomas/sintomas.page.ts
+++ src/app/sintomas/sintomas.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-sintomas',
   templateUrl: './sintomas.page.html',
   styleUrls: ['./sintomas.page.scss'],
 })
-export class SintomasPage implements OnInit {
 
-  constructor() { }
+export class SintomasPage {
+  public form = [
+      { val: 'Febre', isChecked: false },
+      { val: 'Tosse', isChecked: false },
+      { val: 'Cansaço', isChecked: false },
+      { val: 'Perda de paladar', isChecked: false },
+      { val: 'Dor de cabeça', isChecked: false },
+      { val: 'Dores de garganta', isChecked: false }
+    ];
 
-  ngOnInit() {
-  }
+    public entrada = 0;
+
+
+
+
 
 }
+
+
+
+
