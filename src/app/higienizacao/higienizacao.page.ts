f82ba170b0a5457856d262021728769bf54ab698
Fernanda Alipio <62899970+fernandaalipio@users.noreply.github.com>
2021-04-05 20:53:36 -0300

    Update higienizacao.page.ts

--- src/app/higienizacao/higienizacao.page.ts
+++ src/app/higienizacao/higienizacao.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -10,7 +11,51 @@ export class HigienizacaoPage{
   segmentChanged(mudar: any) {
     console.log('Mudança de categoria carregada', mudar);
   }
+
+  public user = {
+    favorito:false,
+  
+  };
+
+  public validarFavorito (){
+    console.log("mudança");
+  
+
+   }
+
+  constructor(public actionSheetController: ActionSheetController) {}
+
+  async presentActionSheet() {
+    const botao = await this.actionSheetController.create({
+      header: 'Selecione a melhor opção',
+      buttons: [ {
+        text: 'Compartilhar',
+        icon: 'share',
+        handler: () => {
+          console.log('Compartilhar');
+        }
+      }, {
+        text: 'Favoritar',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favoritar');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelar');
+        }
+      }]
+    });
+    await botao.present();
+  }
+
+
+   
   
 }
+
 
 
