3ede685bf390f44d8f9dbc928f238105aff970b4
leocotic <81317989+leocotic@users.noreply.github.com>
2021-04-22 17:33:21 -0300

    Update news.page.ts

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jQuery';
+import { getTranslationDeclStmts } from '@angular/compiler/src/render3/view/template';
+import { Component, NgModule, TemplateRef } from '@angular/core';
+import { AlertController } from '@ionic/angular'; // importei alertControler do pactote ionic/ angular
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.page.html',
   styleUrls: ['./news.page.scss'],
 })
-export class NewsPage implements OnInit {
+export class NewsPage {
 
-  constructor() {}
+  constructor(  private alertCtrl: AlertController ) {}
 
-  ngOnInit() {
-
+  async mostrarAlerta(){
+    const myAlert = this.alertCtrl.create({ 
+  
+      subHeader: 'Use Máscara',
+      message: ' Fique em Casa !!!',
+  
+      buttons : ['ok']
+    });
+  
+    (await myAlert).present(); 
+  
+  
   }
 
 }
