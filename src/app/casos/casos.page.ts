807b7041af6faff30bcf8967b0c9b21be650fd86
rrmh <77122434+rrmh@users.noreply.github.com>
2021-05-14 11:52:28 -0300

    optimization

--- src/app/casos/casos.page.ts
+++ src/app/casos/casos.page.ts
@@ -1,4 +1,4 @@
-import { Component , AfterViewInit, ElementRef, ViewChild} from '@angular/core';
+import { Component , OnInit, ElementRef, ViewChild} from '@angular/core';
 import { Chart } from 'chart.js';
 import { Storage } from '@ionic/storage-angular';
 import { ApiCasosService } from '../api-casos.service';
@@ -7,9 +7,9 @@ import { ApiCasosService } from '../api-casos.service';
   templateUrl: './casos.page.html',
   styleUrls: ['./casos.page.scss'],
 })
-export class CasosPage implements AfterViewInit{
+export class CasosPage implements OnInit{
   
-  constructor( public storage: Storage, private dataApi: ApiCasosService) { 
+  constructor( private storage: Storage, private dataApi: ApiCasosService) { 
   }
   @ViewChild('barCanvas') private barCanvas: ElementRef;
   barChart: any;
@@ -23,40 +23,44 @@ export class CasosPage implements AfterViewInit{
   result: {};
 
 
-  ngAfterViewInit() {
-     this.fetchData();
-   }
-
-  async fetchData(){
-
-    this.result = await this.dataApi.getDataApi();
-
-    this.storage.create();
+  ngOnInit() {
+    this.makeCall(this.fetchData);
     
-    for(let i = 0; i<this.result["data"].length; i++){
-      this.storage.set(`${i}`,this.result['data'][i]);
+  }
+
+   async makeCall(callback){
+    if((await this.storage.length()).valueOf() != 0){
+      callback(this.storage,this.casosArrayList,this.labelsUf,this.casosArray,this.casosArray100,this.obitosArray);
+    }else{
+      this.result = await this.dataApi.getDataApi();
+      for(let i = 0; i<this.result["data"].length; i++){
+        this.storage.set(`${i}`,this.result['data'][i]);
+      }
+      
+      callback(this.storage,this.casosArrayList,this.labelsUf,this.casosArray,this.casosArray100,this.obitosArray);
     }
-     
-      this.storage.forEach((x) => {
-        if(x.uf){
-          this.casosArrayList.push(x);
-          this.labelsUf.push(x.uf);
-          this.casosArray.push(x.cases);
-          this.casosArray100.push(x.cases/100);
-          this.obitosArray.push(x.deaths);
-        } 
-      }) 
-    
+  }
 
-    if(this.labelsUf){
-      this.barChartMethod();
-    } 
+  fetchData(hd, cal, lu,ca, ca100, oa){
+    
+    
+    hd.forEach((x) => {
+      if(x.uf){
+        cal.push(x);
+        lu.push(x.uf);
+        ca.push(x.cases);
+        ca100.push(x.cases/100);
+        oa.push(x.deaths);
+      } 
+    }) 
+
+    cal.push({uf:'Casos/Óbitos graph'});
   }
 
   toggleSection(items) {
     
     for(let i = 0; i<this.casosArrayList.length; i++){
-      if(items == this.casosArrayList[i]){
+      if(items == this.casosArrayList[i]){ 
         this.casosArrayList[i].open = !this.casosArrayList[i].open;
         return;
       }
--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,7 +1,6 @@
 import { getTranslationDeclStmts } from '@angular/compiler/src/render3/view/template';
 import { Component, NgModule, TemplateRef } from '@angular/core';
 import { AlertController } from '@ionic/angular'; // importei alertControler do pactote ionic/ angular
-
 @Component({
   selector: 'app-news',
   templateUrl: './news.page.html',
@@ -9,20 +8,22 @@ import { AlertController } from '@ionic/angular'; // importei alertControler do 
 })
 export class NewsPage {
 
-  constructor(  private alertCtrl: AlertController ) {}
+  constructor(private alertCtrl: AlertController ) {}
 
   async mostrarAlerta(){
     const myAlert = this.alertCtrl.create({ 
-  
+      
+      
+
       subHeader: 'Use Máscara',
       message: ' Fique em Casa !!!',
   
       buttons : ['ok']
     });
-  
+   
     (await myAlert).present(); 
   
-  
+    this.storage.forEach((x)=>{console.log(x)});
   }
 
 }
