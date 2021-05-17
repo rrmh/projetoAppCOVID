0e937ee7ba554972a5a6cc25e9279b6376ad445d
Fernanda Alipio <62899970+fernandaalipio@users.noreply.github.com>
2021-05-17 18:50:05 -0300

    Update favoritos.page.ts

--- src/app/favoritos/favoritos.page.ts
+++ src/app/favoritos/favoritos.page.ts
@@ -9,20 +9,41 @@ export class FavoritosPage implements OnInit {
 
   arrayListIm = [];
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage) { 
+    console.log("CONSTRUTOR FAVORITOS");
+  }
 
   ngOnInit() {
+    this.arrayListIm = [];
+
+    console.log("INIT FAVORITOS: " + this.arrayListIm.length);
+
+    this.favIm();
+  }
+  
+  ionViewWillEnter(){
+    this.arrayListIm = [];
+
+    console.log("will enter FAVORITOS" + this.arrayListIm.length);
     this.favIm();
   }
 
   favIm(){
-    
-    this.storage.forEach((x)=>{
-      console.log(/^favIm/.test('favIm2'))
-      if(x.key.match(/^favIm/)){
-        console.log(x.key)
-        console.log('test')
-        this.arrayListIm.push(x);
+    this.storage.length().then(result =>{
+      console.log("Registros no banco de dados: " + result)
+    });
+
+    this.storage.forEach((value, key, index)=>{
+
+      console.log('index: ' +index)
+
+      if(key.match(/^favIm/)){
+        if (!this.arrayListIm.includes(value)) { 
+          console.log("Adcionando chave: " + key)
+
+          this.arrayListIm.push(value);
+
+        }
       }
     })
   }
