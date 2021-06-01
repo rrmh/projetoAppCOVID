7f08316c6869b34dacf7f506f7d0636f7a26eaf3
Fernanda Alipio <62899970+fernandaalipio@users.noreply.github.com>
2021-06-01 13:52:33 -0300

    Update higienizacao.page.ts

--- src/app/higienizacao/higienizacao.page.ts
+++ src/app/higienizacao/higienizacao.page.ts
@@ -13,6 +13,8 @@ export class HigienizacaoPage{
  public contador_favoritos= 0;
   pesquisa: string;
   arrayList = [{}];
+  arrayVideos = [{}];
+  
 
   segmentChanged(mudar: any) {
     console.log('Mudança de categoria carregada', mudar);
@@ -43,18 +45,40 @@ export class HigienizacaoPage{
    
 
   constructor(public actionSheetController: ActionSheetController,  public storage: Storage) {
-    this.arrayList = [{label:'Lavar as Mãos',
+    this.arrayList = [{
+    label:'Lavar as Mãos',
     key:'favIm1',
     url:'https://www.jau.sp.gov.br/uploads/noticias/2020/04/08/195840/covid-preven%C3%A7%C3%A3o.jpg.jpg'
     },
-    {label:'Higienizar as mãos',
+    {
+    label:'Higienizar as mãos',
     key:'favIm2',
-      url:'https://www.laurodefreitas.ba.gov.br/imagens/img_user/1582977967290220jpg'
+   url:'https://www.laurodefreitas.ba.gov.br/imagens/img_user/1582977967290220jpg'
     },
-    {label:'5 medidas para se proteger do COVID',
+    {
+    label:'5 medidas para se proteger do COVID',
     key:'favIm3',
     url:'https://www.princesa.pb.gov.br/storage/content/noticias/geral/3644/destaque/img_202005281518ftTn.jpeg'
     }]
+
+    
+  this.arrayVideos = [
+    {
+    label: 'Higienizando a mão com água e sabão',
+    key: 'favVideo1',
+    url: 'https://s2.glbimg.com/uLRkPHa7Wy5form94a2q8nVq4AE=/620x480/e.glbimg.com/og/ed/f/original/2018/10/08/agua-e-sabao.jpg'
+  },
+  {
+    label: 'Palavras de especialista',
+    key: 'favVideo2',
+    url: 'https://s2.glbimg.com/uLRkPHa7Wy5form94a2q8nVq4AE=/620x480/e.glbimg.com/og/ed/f/original/2018/10/08/agua-e-sabao.jpg'
+  },
+  {
+    label: 'Diferença entre exames RT-PCR e sorologia',
+    key: 'favVideo3',
+    url: 'https://s2.glbimg.com/uLRkPHa7Wy5form94a2q8nVq4AE=/620x480/e.glbimg.com/og/ed/f/original/2018/10/08/agua-e-sabao.jpg'
+  }]
+
   }
   async presentActionSheet(item) {
     const botao = await this.actionSheetController.create({
